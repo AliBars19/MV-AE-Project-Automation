3da013b109aae709a9fe3aea67ec4d5a0a171fcd
AliBars19 <Ali.Bars@city.ac.uk>
2025-11-05 13:58:11 +0000

    Implemented Image download feature

--- main.py
+++ main.py
@@ -1,22 +1,41 @@
-import requests
-import os
-import subprocess 
-import pytube 
-from pytube import YouTube
-
-import json
+from pytube import YouTube # for audio extraction
 import yt_dlp
 
-URLS = 'https://youtu.be/95_ccujUKKc?si=1pWhV94UD2CkZUST'
+import requests # for image extraction
+from PIL import Image
+from io import BytesIO
 
+
+
+
+
+
+mp3URL = str(input("Enter Audio URL"))
+imgurl = str(input("Enter Image URL"))
+
+#------------------------------------------- EXTRACTING AUDIO
+#'''
 ydl_opts = {
-    'format': 'm4a/bestaudio/best',
-    # ℹ️ See help(yt_dlp.postprocessor) for a list of available Postprocessors and their arguments
+    'format': 'mp3/bestaudio/best',
     'postprocessors': [{  # Extract audio using ffmpeg
         'key': 'FFmpegExtractAudio',
-        'preferredcodec': 'm4a',
+        'preferredcodec': 'mp3',
     }]
 }
 
 with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-    error_code = ydl.download(URLS)
+    error_code = ydl.download(mp3URL)
+#'''
+#---------------------------------------- EXTRACTING PNG
+#'''
+if __name__ == '__main__':
+    
+    response = requests.get(imgurl)
+    print(response)
+    if response.status_code == 200:
+        img = Image.open(BytesIO(response.content))
+        img.save('C:/Users/aliba/Downloads/MacbookVisuals/MV-AE-Project-Automation/image.png')
+    else:
+        print("BAD IMAGE LINK")
+#'''
+#--------------------------------------- EXTRACTING COLORS FROM PNG
